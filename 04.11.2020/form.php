7873fd531a45e541bc68c80aa34b7560eb3b77c7
Dorado111 <73247550+Dorado111@users.noreply.github.com>
2020-11-04 13:00:52 +0300

    Create form.php

---
+++ 04.11.2020/form.php
@@ -0,0 +1,10 @@
+<form action="" method="POST">
+    <select name="funcName">
+        <option value="func1">Задание 1</option>
+        <option value="func2">Задание 2</option>
+        <option value="func3">Задание 3</option>
+        <option value="func4">Задание 4</option>
+        <option value="func5">Задание 5</option>
+    </select>
+    <input type="submit" value="Отправить">
+</form>
