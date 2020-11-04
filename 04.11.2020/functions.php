d45ba821b14bcbfabd804f81ed326f822abf8e2e
Dorado111 <73247550+Dorado111@users.noreply.github.com>
2020-11-04 21:50:27 +0300

    Update functions.php

--- 04.11.2020/functions.php
+++ 04.11.2020/functions.php
@@ -1,32 +1,20 @@
 <?php
 echo call_user_func($_POST['funcName'], $_POST['text']);
 
-
 function func1($text)
 {
-    $text = " Летом  1960 года я, в ту    пору студент факультета антропологии при
-                                 Калифорнийском      университете в Лос-Анжелесе,<br>
-        предпринял несколько поездок на    Юго-Запад с целью   сбора информации о
-                     лекарственных растениях     используемых местными индейцами.<br>
-       К одной из этих поездок относится      начало описываемых здесь событий.";
     $tmp = rtrim($text);
     $tmp = preg_replace("/\s{2,}/", ' ', $tmp);
-    $res = mb_strlen($tmp) . '<br>' . '<br>' . $tmp . '<br><br>';
+    $res = strlen($tmp) . '<br>' . '<br>' . $tmp . '<br><br>';
 
     return $res;
 }
 
 function func2($text)
 {
-    $text = " Летом  1960 года я, в ту    пору студент факультета антропологии при
-                                 Калифорнийском      университете в Лос-Анжелесе,<br>
-        предпринял несколько поездок на    Юго-Запад с целью   сбора информации о
-                   лекарственных растениях     используемых местными индейцами.<br>
-       К одной из этих поездок относится      начало описываемых здесь событий.";
-    $text = preg_replace("/\s{2,}/", ' ', $text);
     $res = '';
     foreach (explode(' ', $text) as $words) {
-        $new = mb_strlen($words);
+        $new = strlen($words);
         if ($new >= 3 && $new <= 5) {
             $res .= "$words <br>";
         }
@@ -37,16 +25,12 @@ function func2($text)
 
 function func3($text)
 {
-    $text = "     Mom washed the frame.   ";
     $tmp2 = trim($text);
     $tmp2 = preg_replace("/\s{2,}/", ' ', $tmp2);
-    print_r(mb_strlen($tmp2) . '<br>' . "<br>");
+  
 
-    echo "Вводная строка<br>" . $tmp2 . '<br><br>';
-
-    echo 'Обратная строка ввода с использованием strrev<br>';
     $reverse = strrev($tmp2);
-    echo $reverse . '<br><br>';
+   
 
     $res = ($tmp2 == $reverse) ? $tmp2 . 'Является полиндромом <br>' : $reverse . '|' . 'Не является полиндромом<br><br>';
 
@@ -55,7 +39,6 @@ function func3($text)
 
 function func4($text)
 {
-    $text = "     Mom washed the frame.   ";
     $words1 = explode(' ', $text);
 
     $word1 = 0;
@@ -73,14 +56,9 @@ function func4($text)
 
 function func5($text)
 {
-    $text = " Летом  1960 года я, в ту    пору студент факультета антропологии при
-                                 Калифорнийском      университете в Лос-Анжелесе,<br>
-        предпринял несколько поездок на    Юго-Запад с целью   сбора информации о
-                     лекарственных растениях     используемых местными индейцами.<br>
-       К одной из этих поездок относится      начало описываемых здесь событий.";
     $res = '';
-    $res .= 'Количество прописных букв' . ' - ' . (mb_strlen(preg_replace('/[^A-ZА-ЯЁ]/u','', $text))) . '<br>';
-    $res .= 'Количество строчных букв' . ' - ' . (mb_strlen(preg_replace('/[^a-zа-яё]/u','', $text))) . '<br>';
+    $res .= 'Количество прописных букв' . ' - ' . (strlen(preg_replace('/[^A-ZА-ЯЁ]/u', '', $text))) . '<br>';
+    $res .= 'Количество строчных букв' . ' - ' . (strlen(preg_replace('/[^a-zа-яё]/u', '', $text))) . '<br>';
 
     return $res;
 }
